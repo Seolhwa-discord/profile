003c51a4d1cb6a0b3bfd70dbb78a33037bcff39d
seolhwa-dev <znzsndj@gmail.com>
2025-06-09 23:58:46 +0900

    111111111111111111111111111111111111111111111

---
+++ dist/scripts/main.js
@@ -0,0 +1,25 @@
+"use strict";
+var _a;
+(_a = document.getElementById('copyEmail')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', function (event) {
+    event.preventDefault();
+    const email = 'znzsndj@gmail.com';
+    navigator.clipboard.writeText(email).then(() => {
+        showToast('이메일 주소가 클립보드에 복사되었습니다.');
+    }).catch(err => {
+        showToast('복사 실패.');
+    });
+});
+function showToast(message) {
+    const toast = document.getElementById('toast');
+    if (!toast)
+        return;
+    toast.textContent = message;
+    toast.style.visibility = 'visible';
+    toast.style.opacity = '1';
+    setTimeout(() => {
+        toast.style.opacity = '0';
+        setTimeout(() => {
+            toast.style.visibility = 'hidden';
+        }, 500);
+    }, 2500);
+}
